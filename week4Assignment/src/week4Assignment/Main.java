5399ce9705a7ae678dfabfc9992ea933c3501456
Maynard Peralta <maynperalta@gmail.com>
2025-10-02 22:25:12 -0600

    Formatted for readability.

--- week4Assignment/src/week4Assignment/Main.java
+++ week4Assignment/src/week4Assignment/Main.java
@@ -5,21 +5,20 @@ import java.text.DecimalFormat;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		DecimalFormat decFor = new DecimalFormat("#,##0.00");
-		double userInput;
-		double maxNumber = 0;
-		double minNumber = 0;
-		double numTotal = 0;
-		double numAverage;
+		DecimalFormat decFor = new DecimalFormat("0.00"); // Format grades to 2 decimal places 
+		double userInput, maxNumber, minNumber, numTotal, numAverage;
 		
-		for (int i = 1; i <=10; ++i) {
+		System.out.println("Please enter grade number 1: "); // Assign value to variables  
+		userInput = scnr.nextDouble();
+		numTotal = userInput;
+		minNumber = userInput;
+		maxNumber = userInput;
+		
+		for (int i = 2; i <=10; ++i) {  // Start for loop with grade 2 after variables have value
 			System.out.println("Please enter grade number " + i + ": ");
 			userInput = scnr.nextDouble();
 			if (userInput > maxNumber) {
 				maxNumber = userInput;
-			}
-			if (i == 1) {
-				minNumber = userInput;
 			}
 			if (userInput < minNumber) {
 				minNumber = userInput;
@@ -31,5 +30,8 @@ public class Main {
 		System.out.println("The average score is: " + decFor.format(numAverage));
 		System.out.println("The highest score is: " + decFor.format(maxNumber));
 		System.out.println("The lowest score is: " + decFor.format(minNumber));
+		
+		scnr.close();
 	}
+	
 }
